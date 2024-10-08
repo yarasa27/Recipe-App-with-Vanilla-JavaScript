fb3eff1bbc76e8952ef90c396e463768873f8088
yarasa27 <elcan.abbasoff27@gmail.com>
2024-10-08 21:35:07 +0400

    finish header html/css/js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -13,4 +13,19 @@ if (sessionStorage.getItem("theme")){
     $HTML.dataset.theme = sessionStorage.getItem("theme");
 } else {
     $HTML.dataset.theme = isDark ? "dark" : "light";
-}
+};
+
+let /** {Boolean} */ isPressed = false;
+
+const changeTheme = function () {
+    isPressed = isPressed ? false : true;
+    this.setAttribute("aria-pressed", isPressed);
+    $HTML.setAttribute("data-theme", ($HTML.dataset.theme == "light") ? "dark" : "light");
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+};
+
+window.addEventListener("load", function () {
+    const /**{NodeElement} */ $themeBtn = this.document.querySelector("[data-theme-btn");
+
+    $themeBtn.addEventListener("click", changeTheme);
+});
