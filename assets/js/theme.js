3103602ddd8e0f0c20e6272e5126ce3823701946
yarasa27 <elcan.abbasoff27@gmail.com>
2024-10-08 21:24:17 +0400

    themejs/dark and light mode

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,3 +5,12 @@
  */
 
 "use strict";
+
+const /** {NodeElement} */ $HTML = document.documentElement;
+const /** {Boolean} */ isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (sessionStorage.getItem("theme")){
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
