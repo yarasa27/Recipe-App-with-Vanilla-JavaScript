08bee5b2d71d7e0da446a007699bcc1b047ecf64
yarasa27 <elcan.abbasoff27@gmail.com>
2024-10-11 23:39:11 +0400

    home.js start

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,3 +5,18 @@
  */
 
 "use strict";
+
+// Home page Search
+
+const /** "{NodeElement}" */ $searchField = document.querySelector("[data-search-field]");
+const /** "{NodeElement}" */ $searchBtn = document.querySelector("[data-search-btn");
+
+$searchBtn.addEventListener("click", function () {
+    if($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
+});
+
+// Search submit when press "Enter" key 
+
+$searchField.addEventListener("keydown", e => {
+    if(e.key === "Enter") $searchBtn.click();
+});
