57feaab65719ef1e30f3c30260ee94b353faad86
yarasa27 <elcan.abbasoff27@gmail.com>
2024-10-12 15:55:19 +0400

    grid list

--- assets/js/home.js
+++ assets/js/home.js
@@ -42,3 +42,23 @@ addEventOnElements($tabBtns, "click", function () {
     $lastActiveTabPanel = $currentTabPanel;
     $lastActiveTabBtn = this;
 });
+
+// Navigate Tab with arrow key
+
+addEventOnElements($tabBtns, "keydown", function(e) {
+    const /**{NodeElement} */ $nextElement = this.nextElementSibling;
+    const /**{NodeElement} */ $previousElement = this.$previousElementSibling;
+
+    if (e.key === "ArrowRight" && $nextElement) {
+        this.setAttribute("tabindex", -1);
+        $nextElement.setAttribute("tabindex", 0);
+        $nextElement.focus();
+    } else if (e.key === "ArrowLeft" && $previousElement) {
+        this.setAttribute("tabindex", -1);
+        $previousElement.setAttribute("tabindex", 0);
+        $previousElement.focus();
+    } else if (e.key === "Tab") {
+        this.setAttribute("tabindex", -1);
+        $lastActiveTabBtn.setAttribute("tabindex", 0);
+    }
+});
