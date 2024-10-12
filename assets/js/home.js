b6bb551f5005e0eb7d8eb5899e6b8b40ce477796
yarasa27 <elcan.abbasoff27@gmail.com>
2024-10-12 15:19:49 +0400

    home and global js change words

--- assets/js/home.js
+++ assets/js/home.js
@@ -20,3 +20,25 @@ $searchBtn.addEventListener("click", function () {
 $searchField.addEventListener("keydown", e => {
     if(e.key === "Enter") $searchBtn.click();
 });
+
+/** Tab panel navigation */
+
+const /** {NodeList} */ $tabBtns = document.querySelectorAll("[data-tab-btn]");
+const /** {NodeList} */ $tabPanels = document.querySelectorAll("[data-tab-panel]");
+
+let /** {NodeElement} */ [$lastActiveTabPanel] = $tabPanels;
+let /** {NodeElement} */ [$lastActiveTabBtn] = $tabBtns;
+
+addEventOnElements($tabBtns, "click", function () {
+    $lastActiveTabPanel.setAttribute("hidden", "");
+    $lastActiveTabBtn.setAttribute("aria-selected", false);
+    $lastActiveTabBtn.setAttribute("tabindex", -1);
+
+    const /** {NodeElement} */ $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
+    $currentTabPanel.removeAttribute("hidden");
+    this.setAttribute("aria-selected", true);
+    this.setAttribute("tabindex", 0);
+
+    $lastActiveTabPanel = $currentTabPanel;
+    $lastActiveTabBtn = this;
+});
